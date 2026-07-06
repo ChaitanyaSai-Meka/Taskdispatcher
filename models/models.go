281dec80e426f0d898313f37ed4f308fc6760820
ChaitanyaSai-Meka <chaitanya.s.meka@gmail.com>
2026-07-06 07:17:12 +0530

    remove unused job and server packages; update Task struct to use JobClass and Status types

--- models/models.go
+++ models/models.go
@@ -1,7 +1,23 @@
 package models
 
+type JobClass int 
+const (
+	Class1 JobClass = iota+1
+	Class2
+	Class3
+)
+
+type Status string 
+const (
+	StatusQueued  Status = "queued"
+    StatusRunning Status = "running"
+    StatusDone    Status = "done"
+    StatusFailed  Status = "failed"
+    StatusBusy    Status = "worker_busy"
+)
 type Task struct {
 	ID int `json:"id"`
 	TaskName string `json:"task_name"`
-	Class int `json:"class"`
+	Class JobClass `json:"class"`
+	Status Status `json:"status"`
 }
